3b53104937a4b703131484b32b3d8d01e48cde76
Andrew Thornton <awjdthornton@gmail.com>
2019-08-25 01:34:26 -0700

    merging data sets and using js script to populate chart

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
+//Wrangle NBA data and populate a chart
 
-let regSeasonData = null;
-let playoffsData = null;
+let regSeasonData = [];
+let playoffsData = [];
+let mergedData = [];
 let teamNameMapping = {
                         "Milwaukee Bucks":"MIL",
                         "Toronto Raptors":"TOR",
@@ -20,16 +22,33 @@ let teamNameMapping = {
                         "Indiana Pacers":"IND",
                       };
 
+function parseData (data) {
+  console.log('Data passed to parseData()', data);
+  let headers = data.headers;
+  let rowSet = data.rowSet;
+  let cleanData = [];
+  
+  //combine separate arrays into a single array of objects with headers as keys
+  for (let row of rowSet) {
+    let newObj = {};
+    for (let [i,header] of headers.entries()) {
+      newObj[header] = row[i];
+    }
+    
+    //add team abbreviation for chart label
+    newObj["TEAM_ABR"] = (teamNameMapping[newObj["TEAM_NAME"]]) ? teamNameMapping[newObj["TEAM_NAME"]] : '';
+    cleanData.push(newObj);
+  }
+    
+  return cleanData;
+}
 
 function getRegSeason () {
     fetch("./data/2018_19_RegSeason_Team_Data.json")
     .then(response => response.json())
     .then(data => {
-      console.log("Got the Regular Season data!");
-      
       regSeasonData = parseData(data.resultSets[0]);
       
-      // TODO call a function to do something with this data
       getPlayoffs();
     });
 }
@@ -38,62 +57,53 @@ function getPlayoffs () {
   fetch("./data/2018_19_Playoffs_Team_Data.json")
     .then(response => response.json())
     .then(data => {
-      console.log("Got the Playoff data!");
       
       playoffsData = parseData(data.resultSets[0]);
       
-      // TODO: Call a function to do something with this data
-      render()
+      mergeDataSets();
     });
 }
 
-function render () {
-  // TODO add render function code
-  console.log ('Running render function');
+function mergeDataSets () {
   console.log('regSeasonData ->',regSeasonData);
   console.log('playoffData ->',playoffsData);
+
+  //merge the import elements from the two data sets
+  for (let p of playoffsData) {
+    let mergedObj = {};
+    for (let r of regSeasonData) {
+      if (p.TEAM_ABR === r.TEAM_ABR) {
+        mergedObj["TEAM_ABR"] = p.TEAM_ABR;
+        mergedObj["REG_W_PCT"] = Math.round(r.W_PCT*100);
+        mergedObj["PLYOFF_W_PCT"] = Math.round(p.W_PCT*100);
+      }
+    }
+    mergedData.push(mergedObj);
+  }
   
-  /*
-  Combine the key elements from each data set into an object within an array  that looks like
-  [
-    { "TEAM_ABR": "GSW", 
-      "REG_WIN_PCT": "72.3", 
-      "PLYOFF_WIN_PCT": "65.4",
-    },
-    ...
-  ]
+  console.log('mergedData array ->', mergedData)
   
-  Note: only include playoff teams
+  //TODO Mutate the array to order by regular season winning percentage in DESC order (i.e. highest win percentage to lowest)
   
-  mutate the array to order by regular season winning percentage in DESC order (i.e. highest win percentage to lowest)
+  render();
+}
+
+function render () {
+  console.log ('Running render function');
+
+  let chart = document.querySelector('#chart');
   
-  inject html into DOM for each team with found in this new object
-  
-  */
+  // create the chart content using data from merged data set
+  for (let item of mergedData) {
+    chart.innerHTML += `<div class="Viz-item" onClick="alert('${item["TEAM_ABR"]} won ${item["REG_W_PCT"] - item["PLYOFF_W_PCT"]}% fewer of their games during the playoffs.')">
+                    <div class="Viz-data Viz-regular" style="height: ${item["REG_W_PCT"]}%"><label>${item["REG_W_PCT"]}</label></div>
+                    <div class="Viz-data Viz-playoff" style="height: ${item["PLYOFF_W_PCT"]}%"><label>${item["PLYOFF_W_PCT"]}</label></div>
+                    <div class="Viz-label">${item["TEAM_ABR"]}</div>
+                </div>`
+  }
   
 }
 
-function parseData (data) {
-  console.log('Data passed to parseData()', data);
-  let headers = data.headers;
-  let rowSet = data.rowSet;
-  console.log('headers ->',headers);
-  console.log('rowSet ->',rowSet);
-  let cleanData = [];
-  
-  for (let row of rowSet) {
-    let newObj = {};
-    for (let [i,header] of headers.entries()) {
-//      console.log('header: value', header+':',row[i]);
-      newObj[header] = row[i];
-    }
-    //add team abbreviation for label
-    newObj["TEAM_ABR"] = (teamNameMapping[newObj["TEAM_NAME"]]) ? teamNameMapping[newObj["TEAM_NAME"]] : '';
-    cleanData.push(newObj);
-  }
-    
-  return cleanData;
-}
 
 //initiate data load
 getRegSeason();

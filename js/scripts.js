4c51104ee23489b7cf4fd63d35ae79cb64b495d6
Andrew Thornton <awjdthornton@gmail.com>
2019-08-26 12:22:47 -0700

    adding dimension to chart

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,56 @@
 let regSeasonData = [];
 let playoffsData = [];
 let mergedData = [];
-let teamNameMapping = {
-                        "Milwaukee Bucks":"MIL",
-                        "Toronto Raptors":"TOR",
-                        "Golden State Warriors":"GSW",
-                        "Philadelphia 76ers":"PHI",
-                        "Boston Celtics":"BOS",
-                        "Houston Rockets":"HOU",
-                        "Denver Nuggets":"DEN",
-                        "Portland Trail Blazers":"POR",
-                        "San Antonio Spurs":"SAS",
-                        "LA Clippers":"LAC",
-                        "Brooklyn Nets":"BKN",
-                        "Oklahoma City Thunder":"OKC",
-                        "Orlando Magic":"ORL",
-                        "Utah Jazz":"UTA",
-                        "Detroit Pistons":"DET",
-                        "Indiana Pacers":"IND",
-                      };
+let teamNameConfMapping = [
+                        { "Milwaukee Bucks":"MIL",
+                          "Conference": "Eastern",
+                        },
+                        { "Toronto Raptors":"TOR",
+                          "Conference": "Eastern",
+                        },
+                        { "Golden State Warriors":"GSW",
+                          "Conference": "Western",
+                        },
+                        { "Philadelphia 76ers":"PHI",
+                          "Conference": "Eastern",
+                        },
+                        { "Boston Celtics":"BOS",
+                          "Conference": "Eastern",
+                        },
+                        { "Houston Rockets":"HOU",
+                          "Conference": "Western",
+                        },
+                        { "Denver Nuggets":"DEN",
+                          "Conference": "Western",
+                        },
+                        { "Portland Trail Blazers":"POR",
+                          "Conference": "Western",
+                        },
+                        { "San Antonio Spurs":"SAS",
+                          "Conference": "Western",
+                        },
+                        { "LA Clippers":"LAC",
+                          "Conference": "Western",
+                        },
+                        { "Brooklyn Nets":"BKN",
+                          "Conference": "Eastern",
+                        },
+                        { "Oklahoma City Thunder":"OKC",
+                          "Conference": "Western",
+                        },
+                        { "Orlando Magic":"ORL",
+                          "Conference": "Eastern",
+                        },
+                        { "Utah Jazz":"UTA",
+                          "Conference": "Western",
+                        },
+                        { "Detroit Pistons":"DET",
+                          "Conference": "Eastern",
+                        },
+                        { "Indiana Pacers":"IND",
+                          "Conference": "Eastern",
+                        },
+                      ];
 
 function parseData (data) {
   console.log('Data passed to parseData()', data);
@@ -35,8 +67,15 @@ function parseData (data) {
       newObj[header] = row[i];
     }
     
-    //add team abbreviation for chart label
-    newObj["TEAM_ABR"] = (teamNameMapping[newObj["TEAM_NAME"]]) ? teamNameMapping[newObj["TEAM_NAME"]] : '';
+    //add team abbreviation and conference
+    for (let team of teamNameConfMapping) {
+      //if matching on team name then add new properties of TEAM_ABR and TEAM_CONF
+      if (team[newObj["TEAM_NAME"]]) {
+        newObj["TEAM_ABR"] = team[newObj["TEAM_NAME"]];
+        newObj["TEAM_CONF"] = team["Conference"];
+      }
+    }
+    
     cleanData.push(newObj);
   }
     
@@ -74,6 +113,7 @@ function mergeDataSets () {
     for (let r of regSeasonData) {
       if (p.TEAM_ABR === r.TEAM_ABR) {
         mergedObj["TEAM_ABR"] = p.TEAM_ABR;
+        mergedObj["TEAM_CONF"] = p.TEAM_CONF;
         mergedObj["REG_W_PCT"] = Math.round(r.W_PCT*100);
         mergedObj["PLYOFF_W_PCT"] = Math.round(p.W_PCT*100);
       }
@@ -81,25 +121,33 @@ function mergeDataSets () {
     mergedData.push(mergedObj);
   }
   
-  console.log('mergedData array ->', mergedData)
+  console.log('mergedData ->', mergedData)
   
-  //TODO Mutate the array to order by regular season winning percentage in DESC order (i.e. highest win percentage to lowest)
-  
-  render();
+  render('All');
 }
 
-function render () {
-  console.log ('Running render function');
+function onDimChange () {
+  //get updated dimension value from drop-down
+  let newDim = document.querySelector('#dim1').value;
+  
+  render(newDim)
+}
+
+function render (dim) {
+  console.log ('Running render function with dimensiion ->',dim);
 
   let chart = document.querySelector('#chart');
+  chart.innerHTML = '';
   
   // create the chart content using data from merged data set
   for (let item of mergedData) {
-    chart.innerHTML += `<div class="Viz-item" onClick="alert('${item["TEAM_ABR"]} won ${item["REG_W_PCT"] - item["PLYOFF_W_PCT"]}% fewer of their games during the playoffs.')">
-                    <div class="Viz-data Viz-regular" style="height: ${item["REG_W_PCT"]}%"><label>${item["REG_W_PCT"]}</label></div>
-                    <div class="Viz-data Viz-playoff" style="height: ${item["PLYOFF_W_PCT"]}%"><label>${item["PLYOFF_W_PCT"]}</label></div>
-                    <div class="Viz-label">${item["TEAM_ABR"]}</div>
-                </div>`
+    if (dim === "All" || dim === item["TEAM_CONF"]) {
+      chart.innerHTML += `<div class="Viz-item" onClick="alert('${item["TEAM_ABR"]} won ${item["REG_W_PCT"] - item["PLYOFF_W_PCT"]}% fewer of their games during the playoffs.')">
+                      <div class="Viz-data Viz-regular" style="height: ${item["REG_W_PCT"]}%"><label>${item["REG_W_PCT"]}</label></div>
+                      <div class="Viz-data Viz-playoff" style="height: ${item["PLYOFF_W_PCT"]}%"><label>${item["PLYOFF_W_PCT"]}</label></div>
+                      <div class="Viz-label">${item["TEAM_ABR"]}</div>
+                  </div>`
+    }
   }
   
 }
